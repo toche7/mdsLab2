7f045de4b805225c8015eb012ec2a833cc9e7c95
toche <t.samanchuen.gmail.com>
2024-06-22 02:50:41 +0700

    ready for week2

---
+++ exercise/ex2.py
@@ -0,0 +1,24 @@
+def homework():
+    import numpy as np
+    import pandas as pd
+    import matplotlib.pyplot as plt
+
+    from sklearn.datasets import load_breast_cancer
+    data = load_breast_cancer()
+    X = data.data
+    y = data.target
+    from sklearn.linear_model import LogisticRegression
+    lg = LogisticRegression(max_iter= 10000, penalty = None, solver ="newton-cg")
+    lg.fit(X, y)
+    y_pred = lg.predict(X)
+    R2 = lg.score(X, y)
+    print("R2: ", R2)
+    print("coef: ", lg.coef_[0,0])
+    print("intercept: ", lg.intercept_)
+    print("predict: ", lg.predict(X[0:1]))
+    print('X:', X[0:1])
+    return  lg, R2
+ 
+
+if __name__ == '__main__':
+    print(homework())
--- tests/func_test.py
+++ tests/func_test.py
@@ -1,16 +1,12 @@
-import ex1 as hw
+import ex2 as hw
 import numpy as np
 def test_hwfunc1():
     model, R2 = hw.homework()
-    assert np.allclose(model.coef_, 1.193033644189594, atol=1e-4)
-    assert np.allclose(model.intercept_, -3.89578087831185, atol=1e-4)
+    assert np.allclose(model.coef_[0,0], 3.52576347, atol=1e-4)
+    assert np.allclose(model.intercept_, 29.33199771, atol=1e-4)
 
 
 def test_hwfunc2():
     model, R2 = hw.homework()
-    assert np.allclose(R2, 0.7020315537841397, atol=1e-4)
+    assert np.allclose(R2, 0.984182776801406, atol=1e-4)
   
-def test_hwfunc3():
-    model, R2 = hw.homework()
-    assert np.allclose(model.predict([[7.0]]), [4.4554546], atol=1e-4)
-    assert np.allclose(model.predict([[10.0]]), [8.03455556], atol=1e-4)
